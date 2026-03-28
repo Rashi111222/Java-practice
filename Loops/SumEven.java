6fe3db317dfea36fd6fd674a5f90f1d84cc5c29e
Rashi Karande <rashikarande2214@gmail.com>
2026-03-28 11:26:11 +0530

    Added Sum of even numbers from N terms

---
+++ Loops/SumEven.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class SumEven {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter how many terms: ");
+        int n=sc.nextInt();
+        int sum=0;
+        for(int i=1;i<=n;i++){
+            if(i%2==0){
+                sum+=i;
+            }
+        }
+        System.out.println("Sum of even number from "+n+" is :"+sum);
+    }
+}
