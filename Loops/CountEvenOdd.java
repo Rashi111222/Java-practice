b3c0681ea6bbe5000573f7f325db01ce804b75ef
Rashi Karande <rashikarande2214@gmail.com>
2026-03-29 19:50:41 +0530

    Added count even odd digits

---
+++ Loops/CountEvenOdd.java
@@ -0,0 +1,19 @@
+public class CountEvenOdd {
+    public static void main(String[] args) {
+        int num=123456;
+        int even=0,odd=0;
+
+        while(num!=0){
+            int digit=num%10;
+            if(digit%2==0){
+                even++;
+            }
+            if(digit%2!=0){
+                odd++;
+            }
+            num/=10;
+        }
+        System.out.println("Even digits: "+even);
+        System.out.println("Odd digits: "+odd);
+    }
+}
