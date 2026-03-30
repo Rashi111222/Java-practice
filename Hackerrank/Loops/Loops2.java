24ba7c6ab332c9fe82d9b4e89dbde667c9ec42c4
Rashi Karande <rashikarande2214@gmail.com>
2026-03-30 15:13:46 +0530

    Solved Hackerrank Java Loops 2 program

---
+++ Hackerrank/Loops/Loops2.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class Loops2 {
+    public static void main(String []args){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        int a,b,n;
+        for(int i=0;i<t;i++){
+             a = in.nextInt();
+            b = in.nextInt();
+             n = in.nextInt();
+             
+             int sum=a;
+        for(int j=0;j<n;j++){
+            sum+=(int)Math.pow(2,j)*b;
+            System.out.print(sum+ " ");
+        }
+        System.out.println();
+        }
+        
+        in.close();
+    }
+}
