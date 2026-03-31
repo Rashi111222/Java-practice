057156abd170b28bfea25fbad2c00f9757ba7531
Rashi Karande <rashikarande2214@gmail.com>
2026-03-31 15:26:46 +0530

    Added sum of digits until single digit

---
+++ Loops/SumUntilSingleDigit.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SumUntilSingleDigit{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int num=sc.nextInt();
+
+        while(num>9){
+            int sum=0;
+            while(num!=0){
+                int digit=num%10;
+                sum+=digit;
+                num/=10;
+            }
+
+            num=sum;
+        }
+        System.out.println(num);
+    }
+}
