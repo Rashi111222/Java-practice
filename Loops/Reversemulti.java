8095e2886102abbfbf16e182274ca8788f1967a6
Rashi Karande <rashikarande2214@gmail.com>
2026-04-07 23:54:35 +0530

    Added Reverse Multiplication table

---
+++ Loops/Reversemulti.java
@@ -0,0 +1,9 @@
+public class Reversemulti{
+    public static void main(String[] args) {
+    int num=5;
+
+    for(int i=10;i>=1;i--){
+        System.out.println(num+" x "+i+"="+num*i);
+    }
+}
+}
