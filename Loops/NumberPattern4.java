1a1a44170cdb3a3dd64245566040edee32e8dae7
Rashi Karande <rashikarande2214@gmail.com>
2026-06-01 10:14:15 +0530

    Added half diamond number pattern

---
+++ Loops/NumberPattern4.java
@@ -0,0 +1,12 @@
+public class NumberPattern4 {
+    public static void main(String[] args) {
+        int num=0;
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+                num++;
+                System.out.print(num);
+            }
+            System.out.println();
+        }
+    }
+}
