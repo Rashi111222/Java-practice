8ab364ce62bf787968f78e73fd2b39b7bb888c01
Rashi Karande <rashikarande2214@gmail.com>
2026-07-01 21:25:16 +0530

    Added check if array is sorted

---
+++ Arrays/CheckArraySorted.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class CheckArraySorted {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter how many: ");
+        int n=sc.nextInt();
+
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            System.out.println("Enter "+i+" term");
+            arr[i]=sc.nextInt();
+        }
+
+        System.out.println("array elements: ");
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+
+        boolean isSorted=true;
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i]>arr[i+1]){
+                isSorted=false;
+            }
+        }
+        if(isSorted)
+            System.out.println("Sorted");
+        else
+            System.out.println("Not sorted");
+    }
+}
