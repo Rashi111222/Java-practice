ccdbc76463af2a54f8b90e55f12fb145e0d4238d
Rashi Karande <rashikarande2214@gmail.com>
2026-07-02 23:23:51 +0530

    Remove class files and add gitignore

---
+++ Loops/AdvanceNumberPattern.java
@@ -0,0 +1,15 @@
+public class AdvanceNumberPattern{
+    public static void main(String args[]){
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=2*i-1;j++){
+                if(j%2==0){
+                    System.out.print("0");
+                }
+                else{
+                    System.out.print("1");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
